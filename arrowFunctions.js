de561f646ffd8f87af0f14949030b7db232cef7a
johang2012 <johan.gudmundsson2012@gmail.com>
2016-12-03 21:05:42 +0100

    Added named anonumous function with multiple statements in ES5 syntax

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -6,6 +6,12 @@ const aFunc = () => Math.random() * 100;
 // Named anonumous function with multiple arguments
 const anotherFunc = (argOne, argTwo) => Math.random() * argOne + argTwo;
 
-// Named anonumos function with one arguments
+// Named anonumous function with one arguments
 const yetAnotherFunc = argOne => Math.random() * argOne;
+
+// Named anonumous function with mutiple statements
+var mutltiFunc = function() {
+  if (true) {
+    // Do something
+  }
 }
