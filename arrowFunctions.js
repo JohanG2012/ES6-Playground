a79b78f5f6d5d2b3a95105a7cef181ae4358c4f8
johang2012 <johan.gudmundsson2012@gmail.com>
2016-12-03 20:51:59 +0100

    Added named anonumous function with one argument in ES5 syntax

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -3,5 +3,10 @@
 // Named anonumous function without any arguments
 const aFunc = () => Math.random() * 100;
 
-// Names anonumous function with multiple arguments
+// Named anonumous function with multiple arguments
 const anotherFunc = (argOne, argTwo) => Math.random() * argOne + argTwo;
+
+// Named anonumos function with one arguments
+var yetAnotherFunc = function(argOne) {
+  return Math.random() * argOne;
+}
