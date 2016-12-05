3c8715893a0140f0a61d38b205e1810c52ac76be
johang2012 <johan.gudmundsson2012@gmail.com>
2016-12-05 23:45:47 +0100

    Added example of let, parameters and functions

--- blockLevelScoping.js
+++ blockLevelScoping.js
@@ -9,6 +9,8 @@ function doAlot() {
   if (true) {
     let aValue = 21;
     console.log(aValue); // 21
+    // This is reffered to as shadowing because aValue in the if statement
+    // "shadows" the aValue in the outer scope
   }
 
   console.log(aValue); // 15
@@ -59,3 +61,17 @@ for (let i = 0, i < 10; i++) {
 }
 
 console.log(i); // ReferenceError
+
+
+// it is not possible to create let variables that has the same name as
+// a parameter
+function sayHello(hello) {
+  let hello = "hi"; // TypeError
+
+  if (true) {
+    // I'm "shadows" the outer scope
+    let hello = "hi";
+    console.log(hello); // "hi"
+  }
+  console.log(hello); // parameter value
+}
