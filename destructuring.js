6f22264a7ba13d09e6c2aa75e03d66da92c18990
johang2012 <johan.gudmundsson2012@gmail.com>
2016-12-06 00:35:42 +0100

    Added array the ES5 way

--- destructuring.js
+++ destructuring.js
@@ -5,3 +5,8 @@ let config {info: "hello", count: 25, isTrue: false},
 console.log(info);
 console.log(count);
 console.log(isTrue);
+
+var anArray = [1, "hello", false];
+var first = anArray[0];
+var second = anArray[1];
+var third = anArray[2];
