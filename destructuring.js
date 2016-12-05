43c159b5938b744df17c0029f753705eae189c79
johang2012 <johan.gudmundsson2012@gmail.com>
2016-12-06 00:57:21 +0100

    Destructuring also supports nesting

--- destructuring.js
+++ destructuring.js
@@ -1,10 +1,21 @@
 // ES6 allows easier extraction of values from a object using destructuring
-let config {info: "hello", count: 25, isTrue: false},
+let config = {info: "hello", count: 25, isTrue: false},
 {info, count, isTrue} = config;
 
 console.log(info);
 console.log(count);
 console.log(isTrue);
+
+// Destructuring also supports nesting
+let person = {name: "Rob",body: {legs: 4}},
+{
+  body: {legs: legs},
+  name
+} = person;
+
+// "Rob" 4
+console.log(name, legs);
+
 
 // ES6 destructuring also works on arrays
 let anArray = [1, "hello", false],
