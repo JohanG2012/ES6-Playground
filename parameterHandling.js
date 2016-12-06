574227136f1ac00858292824a8573e6bed255f7d
johang2012 <johan.gudmundsson2012@gmail.com>
2016-12-06 03:00:15 +0100

    refactor to ES6

--- parameterHandling.js
+++ parameterHandling.js
@@ -1,3 +1,6 @@
+// In ES6 functions bodies does not have to deal with default logic
+// the new syntax is also alot easier to understand
+// parameters with default values are concidered as optional
 function getData(data, useCache=true) {
   if (useCache) {
     console.log("Uses cache for", data);
