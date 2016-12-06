cc2b31b14e192fdbf7181d9dd122b1e3c15f104c
johang2012 <johan.gudmundsson2012@gmail.com>
2016-12-06 06:23:50 +0100

    Added computed property value the ES5 way

--- enhancedObjectLiterals.js
+++ enhancedObjectLiterals.js
@@ -3,11 +3,21 @@
 
 // ES6 introduces a shorthand for values. It lets us shorten intializaton of a
 // property within a object literal if the property key matches an already
-// exsisting variable name
+// exsisting variable name.
+// NOTE: A refferenceError will be thrown if the JS Engine can not find a variable
+// with the same name within the containing scope.
 function Person(name, age, parents) {
   return {
     make,
     model,
     value
   };
+}
+
+function Car(make, model, value) {
+  var car = {};
+
+  car["make" + make] = true;
+
+  return car;
 }
