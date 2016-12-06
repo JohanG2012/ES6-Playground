7cd74a556d755e4aba7ab6739d0634b39e742ef1
johang2012 <johan.gudmundsson2012@gmail.com>
2016-12-06 06:06:05 +0100

    Refactor to ES6

--- enhancedObjectLiterals.js
+++ enhancedObjectLiterals.js
@@ -1,10 +1,13 @@
 "use strict";
 
 
+// ES6 introduces a shorthand for values. It lets us shorten intializaton of a
+// property within a object literal if the property key matches an already
+// exsisting variable name
 function Person(name, age, parents) {
   return {
-    make: make,
-    model: model,
-    value: value
+    make,
+    model,
+    value
   };
 }
