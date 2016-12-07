6f9484b800aed9a4f32a05c63fadd061ca0eb87d
johang2012 <johan.gudmundsson2012@gmail.com>
2016-12-07 21:24:47 +0100

    Refactor to ES6

--- classes.js
+++ classes.js
@@ -26,4 +26,17 @@ const Person = function(age, name, parents) {
 console.log(typeof.Person === "function"); // true
 
 
-Person.prototype = new Student();
+// In ES6 it is much easier to extand a class.
+class Teacher extends Person {
+  constructor(students, course, age, name, parents) {
+    // constructor in the super class must first be called
+    super(age, name, parents);
+    this.students = students;
+    this.course = course;
+  }
+}
+
+// If you dont have a constructor, you dont have to use this.
+class Teacher extends Person {
+  
+}
