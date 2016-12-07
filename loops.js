b315e5e1d920ab44529f0c670efd73724d12e279
johang2012 <johan.gudmundsson2012@gmail.com>
2016-12-07 20:30:52 +0100

    refactor to the ES6 way using for-of loops

--- loops.js
+++ loops.js
@@ -2,8 +2,12 @@
 
 // Iterate over an array
 // Should not be done with a for in loop because it itirate in an arbitary order.
-var arr = [1, 2, 3, 4, 5, 6, 7];
+// Could be done with forEach but other array-like object do not have forEach
+// example nodeList or arguments.
+// ES6 brings the best solution with for-of loops. The purpuse of for-of loops is to
+// iterate over arrays while for-in loops purpose is to iterate over objects
+let arr = [1, 2, 3, 4, 5, 6, 7];
 
-arr.forEach(function (value, i) {
+for (let value of list) {
   console.log(value);
-});
+}
