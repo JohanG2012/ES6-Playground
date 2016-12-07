07299a8352a1c8047650adf2879a19e3b00f2ae4
johang2012 <johan.gudmundsson2012@gmail.com>
2016-12-07 20:25:21 +0100

    Refactor to the ES5 way using forEach

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 "use strict";
 
-var arr = [1, 2, 3, 4, 5, 6, 7],
-length = arr.length,
-i;
+// Iterate over an array
+// Should not be done with a for in loop because it itirate in an arbitary order.
+var arr = [1, 2, 3, 4, 5, 6, 7];
 
-for (i = 0; i < length; i++) {
-  console.log(list[i]);
-}
+arr.forEach(function (value, i) {
+  console.log(value);
+});
