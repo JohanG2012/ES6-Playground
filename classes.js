875919af22ccfa04a3038c125756fc260f00b6ce
johang2012 <johan.gudmundsson2012@gmail.com>
2016-12-07 21:16:34 +0100

    Added more example of es6 classes

--- classes.js
+++ classes.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// ES6 class makes it easier to create and manage classes and also makes them
+// more readable and understandable.
 class Person {
   constructor(age, name, parents) {
     this.age = age;
@@ -7,3 +9,18 @@ class Person {
     this.parents = parents;
   }
 }
+
+// A class does not need a constructor
+class Student {
+
+}
+
+// This is how "class" would look like without syntactic sugar
+const Person = function(age, name, parents) {
+  this.age = age;
+  this.name = name;
+  this.parents = parents;
+}
+
+// ES6 clases are still functions
+console.log(typeof.Person === "function"); // true
