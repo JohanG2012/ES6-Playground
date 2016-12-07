eea1b39f9ff5d1e33ab16f7a810ef2ed0ffa4d43
johang2012 <johan.gudmundsson2012@gmail.com>
2016-12-07 20:41:36 +0100

    Added new files

--- loops.js
+++ loops.js
@@ -6,6 +6,8 @@
 // example nodeList or arguments.
 // ES6 brings the best solution with for-of loops. The purpuse of for-of loops is to
 // iterate over arrays while for-in loops purpose is to iterate over objects
+// for-of loops can also be used to iterate over a string or an array-like objects like
+// nodeList and arguments
 let arr = [1, 2, 3, 4, 5, 6, 7];
 
 for (let value of list) {
