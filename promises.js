1f3adca7bd30db1f6015074db2b8e622d05be2d8
johang2012 <johan.gudmundsson2012@gmail.com>
2016-12-08 04:50:22 +0100

    added example of asynchronous programming in JavaScript with event handlers

--- promises.js
+++ promises.js
@@ -1,1 +1,30 @@
 "use strict";
+
+// Example of asynchronous programming in JavaScript with event handlers
+var url = "/json/data.json";
+var req = new XMLHttpRequest();
+
+request.open("GET", url);
+
+request.onload = function() {
+  if (request.status === 200) {
+    try {
+      var responseJson = JSON.parse(request.responseText);
+      // Do some stuff with the data
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    console.error("Request failed");
+  }
+};
+
+request.oneror = function(e) {
+  console.error(error);
+}
+
+try {
+  request.send();
+} catch (error) {
+  console.log(error)
+}
