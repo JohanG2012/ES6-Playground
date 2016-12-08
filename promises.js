96a8583cefb4bfbb13f529c1e7cf520d74ffaabf
johang2012 <johan.gudmundsson2012@gmail.com>
2016-12-08 05:24:23 +0100

    refactor to promises

--- promises.js
+++ promises.js
@@ -1,44 +1,58 @@
 "use strict";
 
-// Example of asynchronous programming in JavaScript callbacks
-function fetch(url, callback) {
-  var request = new XMLHttpRequest();
+// Example of asynchronous programming in JavaScript using promises
+function fetch(url) {
 
-  request.open("GET", url);
+  // Return promis object
+  // The function in the Promise constructor is known as a executor.
+  // If  computation went well, the executor send the results trough resolve().
+  // If the computation did not go fell, the executor informs the user trough reject().
+  return new Promise((resolve, reject) => {
+    let request = new XMLHttpRequest();
 
-  request.onload = function() {
-    var data;
-    var error;
+    request.open("GET", url);
 
-    if (request.status === 200) {
-      data = request.responseText;
-    } else {
-      error = new Error("Request failed");
-    }
-
-    callback({data:data, error:error});
-  }
-
-  request.oneror = function(error) {
-    callback({error:error});
-  }
-
-  try {
+    request.onload = function() {
+      if (request.status === 200) {
+        //Furfill promise
+        resolve(request.responseText);
+      } else {
+        // reject promise
+        // By throwing an arror we can make use of stack trace
+        reject(new Error("request failed"));
+      }
+    };
     request.send();
-  } catch(error) {
-    callback({error:error})
-  }
+  })
 }
 
-fetch('/json/data.json', function(responseObj) {
-	if (!responseObj.error) {
-		try {
-			console.log('data!', JSON.parse(responseObj.data));
-		}
-		catch (e) {
-      console.error(e);
-    }
-	} else {
-    console.error(responseObj.error);
-  }
-});
+fetch("/json/data.json")
+  .then(response => {
+    let data = JSON.parse(response.text);
+    console.log("main",data);
+
+    return fetch(data.url);
+  })
+  .then(response => {
+    console.log("inner data", response);
+  })
+  .catch(error => {
+    // Catch all errors
+    console.error(error);
+  })
+
+/*
+A promise can have 3 different states.
+
+Unsettled/pending - the promise async operation has not computed its result yet
+Settled: fulfilles - the promise async operation has completed successfully
+Settled: rejected - The promises async operation did not complete sucessfully
+
+A promise can only transition from Unsettled to Settled one time.
+
+The convention for promises do say that you should only use then() for fulfillments
+and catch() exclusibly for errors.
+
+If you call then() on a already fullfilled promise you will recive the cached settled value right away
+
+*/
