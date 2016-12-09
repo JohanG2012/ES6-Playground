cbaf630399bde16e7eed15fe9ad015a1c64a48af
johang2012 <johan.gudmundsson2012@gmail.com>
2016-12-09 05:04:22 +0100

    Added example of Object methods

---
+++ objectMethods.js
@@ -0,0 +1,16 @@
+// The Object.is() method determines whether two values are the same value.
+Object.is('foo', 'foo');     // true
+Object.is(window, window);   // true
+
+Object.is('foo', 'bar');     // false
+Object.is([], []);           // false
+
+var test = { a: 1 };
+Object.is(test, test);       // true
+
+Object.is(null, null);       // true
+
+// Special Cases
+Object.is(0, -0);            // false
+Object.is(-0, -0);           // true
+Object.is(NaN, 0/0);         // true
